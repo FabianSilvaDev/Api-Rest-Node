66c859a6382f17905141b0b0d83682d7e0f59411
FabianSilvaDev <produccionesbog@gmail.com>
2025-10-15 18:13:54 -0500

    integration of validation with ZOD a node property dependence

--- api.js
+++ api.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const crypo = require('node:crypto')
 const app = express()
-const movies = require('./movies.json')
+const movies = require('./movies.json');
+const { validateMovie, validatePartialMovie } = require('./validate.js')
+
 
 const PORT = process.env.PORT ?? 3312;
 
 app.disable('x-powered-by') // desabilita el header x-powered-by
+
+// middleware
+app.use(express.json())
 
 // endpoints are path where there is a sourse
 
@@ -36,6 +42,40 @@ app.get('/movies/:id/', (req,res)=>{ //path-to-regexp
 
 app.get('/movies/:gender',(req,res)=>{
     const {genre} = req.query
+    req.statusCode = 200
+    console.log(req.statusCode)
+    console.log('_______________')
+    if(genre){
+        const filteredMovies = movies.filter(
+            movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
+        )
+        return res.json(filteredMovies)
+    }
+    res.status(404).json({message: 'Movie not found for that genre'})
+})
+
+app.post('/movies', (req, res) => {
+  const result = validateMovie(req.body)
+
+  if (!result.success) {
+    // 422 Unprocessable Entity
+    return res.status(404).json({ error: JSON.parse(result.error.message) })
+  }
+  req.statusCode = 201
+  console.log(req.statusCode)
+  console.log('movie created')
+  console.log('_______________')
+  // en base de datos
+  const newMovie = {
+    id: crypto.randomUUID(), // uuid v4
+    ...result.data
+  }
+
+  // Esto no sería REST, porque estamos guardando
+  // el estado de la aplicación en memoria
+  movies.push(newMovie)
+
+  res.status(201).json(newMovie)
 })
 
 app.listen(PORT,()=>{
